d46d8b7e240679deb6105486f347351709239521
Jonathan Wilson <wilsonwjonathan@gmail.com>
2021-09-05 15:32:55 -0700

    added Match class for parsing match input and calculating match points

--- soccer_game_logic/main.py
+++ soccer_game_logic/main.py
@@ -3,6 +3,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("SoccerMatchResults")
 
+
 class Input:
     def __init__(self, input_file):
         logger.debug(f"Processing file:{input_file}")
@@ -11,3 +12,27 @@ class Input:
     def __iter__(self):
         with open(self.input_file, "r") as f:
             yield from f.readlines()
+
+
+class Match:
+    def __init__(self, raw_match_data):
+        logger.debug(f"Raw Match Data: {raw_match_data}")
+        self.raw_match_data = raw_match_data
+        self.match_data = self.parse_raw_match_data()
+        logger.debug(f"Processed Match Date: {self.match_data}")
+
+    def parse_raw_match_data(self):
+        return {
+            x.strip()[: x.strip().rfind(" ")]: int(x.strip()[x.strip().rfind(" ") + 1])
+            for x in self.raw_match_data.strip().split(",")
+        }
+
+    @property
+    def match_result(self):
+        scores = set(self.match_data.values())
+        if len(scores) == 1:
+            return {team: 1 for team in self.match_data}
+
+        return {
+            team: (score == max(scores)) * 3 for team, score in self.match_data.items()
+        }
